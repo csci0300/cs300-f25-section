f0bc484475980bab1e0872454cf5b99e854e7ada
Nicholas DeMarinis <ndemarinis@wpi.edu>
2025-11-13 21:09:12 +0000

    sec7:  Added starter program.

---
+++ sec7/starter.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+int main() {
+    int pfd[2];
+    pipe(pfd); // create pipe
+
+    pid_t pid = fork();
+
+    if (pid == 0) { // Child
+	// As child process, how to send x via the pipe?
+	int x[2] = {42, 125}; 
+
+
+	exit(0);	
+    } else { // parent
+	// How to read array from pipe?
+
+
+	int status;
+	waitpid(pid, &status, 0);
+	printf("[parent] Child exited with status %d\n", WEXITSTATUS(status));
+    }
+    
+
+    return 0;
+}
